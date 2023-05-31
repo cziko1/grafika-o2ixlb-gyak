369adbd9f5724381567a805ca951b1fd3d890fff
Tivadar Cziko <72433224+cziko1@users.noreply.github.com>
2023-05-31 21:56:21 +0200

    TRAIN
    - The train is moving

--- FelevesBeadando/src/train.c
+++ FelevesBeadando/src/train.c
@@ -8,12 +8,17 @@ void init_train(Train* train)
 
 void render_train(Train* train)
 {
+    static float unit = 0.0;
+
     glPushMatrix();
     glBindTexture(GL_TEXTURE_2D, train->train_texture_id);
     glTranslatef( -5, 5, 0.1);
     glScalef(1.0, 0.7, 0.5);
     glRotated(90, 1, 0, 0);
     
+    glTranslated( 0, 0, unit);  
     draw_model(&(train->train));
     glPopMatrix();
+
+    unit += 0.01;
 }
